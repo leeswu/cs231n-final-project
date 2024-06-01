d65d7db89a825a9bc71b464c26665c1441a6fca8
Sophie Wu <swu.lee02@gmail.com>
2024-06-01 14:17:21 -0700

    change config, add train list files for ucf

--- configs/recognition/tsn/tsn_ucf101.py
+++ configs/recognition/tsn/tsn_ucf101.py
@@ -1,101 +1,33 @@
 _base_ = [
-    '../../_base_/models/tsn_r50.py', '../../_base_/schedules/sgd_100e.py',
-    '../../_base_/default_runtime.py'
+    'tsn_imagenet-pretrained-r50_8xb32-1x1x3-100e_kinetics400-rgb.py'  # inherit template config
 ]
 
 # model settings
 model = dict(
     cls_head=dict(
         type='TSNHead',
-        num_classes=101  # change from 400 to 101
-        ))
+        num_classes=101))  # change from 400 to 101
+
 
 # dataset settings
 dataset_type = 'VideoDataset'
-data_root = 'data/ucf101/videos/'
-data_root_val = 'data/ucf101/videos/'
-ann_file_train = 'data/ucf101/annotations/trainlist01.txt'
-ann_file_val = 'data/ucf101/annotations/trainlist02.txt'
-
-file_client_args = dict(io_backend='disk')
-
-train_pipeline = [
-    dict(type='DecordInit', **file_client_args),
-    dict(type='SampleFrames', clip_len=1, frame_interval=1, num_clips=3),
-    dict(type='DecordDecode'),
-    dict(type='Resize', scale=(-1, 256)),
-    dict(
-        type='MultiScaleCrop',
-        input_size=224,
-        scales=(1, 0.875, 0.75, 0.66),
-        random_crop=False,
-        max_wh_scale_gap=1),
-    dict(type='Resize', scale=(224, 224), keep_ratio=False),
-    dict(type='Flip', flip_ratio=0.5),
-    dict(type='FormatShape', input_format='NCHW'),
-    dict(type='PackActionInputs')
-]
-val_pipeline = [
-    dict(type='DecordInit', **file_client_args),
-    dict(
-        type='SampleFrames',
-        clip_len=1,
-        frame_interval=1,
-        num_clips=3,
-        test_mode=True),
-    dict(type='DecordDecode'),
-    dict(type='Resize', scale=(-1, 256)),
-    dict(type='CenterCrop', crop_size=224),
-    dict(type='FormatShape', input_format='NCHW'),
-    dict(type='PackActionInputs')
-]
-test_pipeline = [
-    dict(type='DecordInit', **file_client_args),
-    dict(
-        type='SampleFrames',
-        clip_len=1,
-        frame_interval=1,
-        num_clips=25,
-        test_mode=True),
-    dict(type='DecordDecode'),
-    dict(type='Resize', scale=(-1, 256)),
-    dict(type='TenCrop', crop_size=224),
-    dict(type='FormatShape', input_format='NCHW'),
-    dict(type='PackActionInputs')
-]
+data_root = 'data/ucf101/videos_train/'
+data_root_val = 'data/ucf101/videos_val/'
+ann_file_train = 'data/ucf101/ucf101_train_list.txt'
+ann_file_val = 'data/ucf101/ucf101_val_list.txt'
 
 train_dataloader = dict(
-    batch_size=32,
-    num_workers=2,
-    persistent_workers=True,
-    sampler=dict(type='DefaultSampler', shuffle=True),
     dataset=dict(
-        type=dataset_type,
         ann_file=ann_file_train,
-        data_prefix=dict(video=data_root),
-        pipeline=train_pipeline))
+        data_prefix=dict(video=data_root)))
 val_dataloader = dict(
-    batch_size=32,
-    num_workers=2,
-    persistent_workers=True,
-    sampler=dict(type='DefaultSampler', shuffle=False),
     dataset=dict(
-        type=dataset_type,
         ann_file=ann_file_val,
-        data_prefix=dict(video=data_root_val),
-        pipeline=val_pipeline,
-        test_mode=True))
+        data_prefix=dict(video=data_root_val)))
 test_dataloader = dict(
-    batch_size=1,
-    num_workers=2,
-    persistent_workers=True,
-    sampler=dict(type='DefaultSampler', shuffle=False),
     dataset=dict(
-        type=dataset_type,
         ann_file=ann_file_val,
-        data_prefix=dict(video=data_root_val),
-        pipeline=test_pipeline,
-        test_mode=True))
+        data_prefix=dict(video=data_root_val)))
 
 train_cfg = dict(
     type='EpochBasedTrainLoop',
@@ -105,7 +37,6 @@ train_cfg = dict(
 val_cfg = dict(type='ValLoop')
 test_cfg = dict(type='TestLoop')
 
-# learning policy
 param_scheduler = [
     dict(
         type='MultiStepLR',
@@ -116,7 +47,6 @@ param_scheduler = [
         gamma=0.1)
 ]
 
-# optimizer
 optim_wrapper = dict(
     optimizer=dict(
         type='SGD',
@@ -125,19 +55,5 @@ optim_wrapper = dict(
         weight_decay=0.0001),
     clip_grad=dict(max_norm=40, norm_type=2))
 
-val_evaluator = dict(type='AccMetric')
-test_evaluator = val_evaluator
-
-default_hooks = dict(checkpoint=dict(interval=3, max_keep_ckpts=3))
-
-# Default setting for scaling LR automatically
-#   - `enable` means enable scaling LR automatically
-#       or not by default.
-#   - `base_batch_size` = (8 GPUs) x (32 samples per GPU).
-auto_scale_lr = dict(enable=False, base_batch_size=256)
-
 # use the pre-trained model for the whole TSN network
 load_from = 'https://download.openmmlab.com/mmaction/v1.0/recognition/tsn/tsn_imagenet-pretrained-r50_8xb32-1x1x3-100e_kinetics400-rgb/tsn_imagenet-pretrained-r50_8xb32-1x1x3-100e_kinetics400-rgb_20220906-cd10898e.pth'
-
-
-
