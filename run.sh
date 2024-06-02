b85816961dad6e298b4c9557b900ccce173e3435
Sophie Wu <swu.lee02@gmail.com>
2024-06-02 01:29:36 -0700

    revert run.sh

--- run.sh
+++ run.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-# # Run training
-# python tools/train.py configs/recognition/tsn/tsn_imagenet-pretrained-r50_8xb32-1x1x3-100e_kinetics400-rgb.py
+# Run training
+python tools/train.py configs/recognition/tsn/tsn_imagenet-pretrained-r50_8xb32-1x1x3-100e_kinetics_dance.py
 
-# # Run Testing
-# python tools/test.py configs/recognition/tsn/tsn_imagenet-pretrained-r50_8xb32-1x1x3-100e_kinetics400-rgb.py \
-#     work_dirs/tsn_imagenet-pretrained-r50_8xb32-1x1x3-100e_kinetics400-rgb/best_acc_*.pth
-
-sleep 10
+# Run Testing
+python tools/test.py configs/recognition/tsn/tsn_imagenet-pretrained-r50_8xb32-1x1x3-100e_kinetics_dance.py \
+    work_dirs/configs/recognition/tsn/tsn_imagenet-pretrained-r50_8xb32-1x1x3-100e_kinetics_dance.py/best_acc_*.pth
 
 # Shut down the VM after the training is complete
 sudo shutdown -h now
