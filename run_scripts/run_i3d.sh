998bd15c0ad3ddf793c1818dc403081d050200cd
Sophie Wu <swu.lee02@gmail.com>
2024-06-02 18:35:27 -0700

    fix file path in run script

--- run_scripts/run_i3d.sh
+++ run_scripts/run_i3d.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Run training
-python tools/train.py configs/recognition/i3d/i3d_dance.py
+python tools/train.py ../configs/recognition/i3d/i3d_dance.py
 
 # Run Testing
-python tools/test.py configs/recognition/i3d/i3d_dance.py \
-    work_dirs/i3d_dance.py/best_acc_*.pth
+python tools/test.py ../configs/recognition/i3d/i3d_dance.py \
+    ../work_dirs/i3d_dance.py/best_acc_*.pth
 
 # Shut down the VM after the training is complete
 sudo shutdown -h now
