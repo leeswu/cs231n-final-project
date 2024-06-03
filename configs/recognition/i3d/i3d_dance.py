aa4b10c9e1d25fd4e09f72a28723f82557cfb2d7
Sophie Wu <swu.lee02@gmail.com>
2024-06-02 18:48:31 -0700

    add i3d ckpt file

--- configs/recognition/i3d/i3d_dance.py
+++ configs/recognition/i3d/i3d_dance.py
@@ -12,6 +12,10 @@ ann_file_train = 'data/k700-2020/updated_splits/small_train.txt'
 ann_file_val = 'data/k700-2020/updated_splits/small_val.txt'
 ann_file_test = 'data/k700-2020/updated_splits/small_test.txt'
 
+model = dict(
+    cls_head=dict(num_classes=19))
+load_from = 'i3d_imagenet-pretrained-r50_8xb8-32x2x1-100e_kinetics400-rgb_20220812-e213c223.pth'
+
 file_client_args = dict(io_backend='disk')
 train_pipeline = [
     dict(type='DecordInit', **file_client_args),
