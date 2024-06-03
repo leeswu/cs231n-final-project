109c0963dc03ba19c3d8efca41c1964d2c331939
Sophie Wu <swu.lee02@gmail.com>
2024-06-02 18:25:25 -0700

    add i3d dance config

--- configs/recognition/i3d/i3d_dance.py
+++ configs/recognition/i3d/i3d_dance.py
@@ -1,70 +1,65 @@
 _base_ = [
-    '../../_base_/models/tsn_r50.py', '../../_base_/schedules/sgd_100e.py',
+    '../../_base_/models/i3d_r50.py', '../../_base_/schedules/sgd_100e.py',
     '../../_base_/default_runtime.py'
 ]
 
 # dataset settings
 dataset_type = 'VideoDataset'
-data_root = 'data/kinetics700/updated_splits/train'
-data_root_val = 'data/kinetics700/updated_splits/val'
-data_root_test = 'data/kinetics700/updated_splits/test'
-ann_file_train = 'data/kinetics700/updated_splits/train.txt'
-ann_file_val = 'data/kinetics700/updated_splits/val.txt'
-ann_file_test = 'data/kinetics700/updated_splits/test.txt'
-
-model = dict(
-    cls_head=dict(num_classes=19))
-load_from = 'https://download.openmmlab.com/mmaction/v1.0/recognition/tsn/tsn_imagenet-pretrained-r50_8xb32-1x1x3-100e_kinetics400-rgb/tsn_imagenet-pretrained-r50_8xb32-1x1x3-100e_kinetics400-rgb_20220906-cd10898e.pth'
+data_root = 'data/k700-2020/updated_splits/small_train'
+data_root_val = 'data/k700-2020/updated_splits/small_val'
+data_root_test = 'data/k700-2020/updated_splits/small_test'
+ann_file_train = 'data/k700-2020/updated_splits/small_train.txt'
+ann_file_val = 'data/k700-2020/updated_splits/small_val.txt'
+ann_file_test = 'data/k700-2020/updated_splits/small_test.txt'
 
 file_client_args = dict(io_backend='disk')
-
 train_pipeline = [
     dict(type='DecordInit', **file_client_args),
-    dict(type='SampleFrames', clip_len=1, frame_interval=1, num_clips=3),
+    dict(type='SampleFrames', clip_len=32, frame_interval=2, num_clips=1),
     dict(type='DecordDecode'),
     dict(type='Resize', scale=(-1, 256)),
     dict(
         type='MultiScaleCrop',
         input_size=224,
-        scales=(1, 0.875, 0.75, 0.66),
+        scales=(1, 0.8),
         random_crop=False,
-        max_wh_scale_gap=1),
+        max_wh_scale_gap=0),
     dict(type='Resize', scale=(224, 224), keep_ratio=False),
     dict(type='Flip', flip_ratio=0.5),
-    dict(type='FormatShape', input_format='NCHW'),
+    dict(type='FormatShape', input_format='NCTHW'),
     dict(type='PackActionInputs')
 ]
 val_pipeline = [
     dict(type='DecordInit', **file_client_args),
     dict(
         type='SampleFrames',
-        clip_len=1,
-        frame_interval=1,
-        num_clips=3,
+        clip_len=32,
+        frame_interval=2,
+        num_clips=1,
         test_mode=True),
     dict(type='DecordDecode'),
     dict(type='Resize', scale=(-1, 256)),
     dict(type='CenterCrop', crop_size=224),
-    dict(type='FormatShape', input_format='NCHW'),
+    dict(type='FormatShape', input_format='NCTHW'),
     dict(type='PackActionInputs')
 ]
 test_pipeline = [
     dict(type='DecordInit', **file_client_args),
     dict(
         type='SampleFrames',
-        clip_len=1,
-        frame_interval=1,
-        num_clips=25,
+        clip_len=32,
+        frame_interval=2,
+        num_clips=10,
         test_mode=True),
     dict(type='DecordDecode'),
     dict(type='Resize', scale=(-1, 256)),
-    dict(type='TenCrop', crop_size=224),
-    dict(type='FormatShape', input_format='NCHW'),
+    dict(type='ThreeCrop', crop_size=256),
+    dict(type='FormatShape', input_format='NCTHW'),
     dict(type='PackActionInputs')
 ]
 
 train_dataloader = dict(
-    batch_size=32,
+    batch_size=8,
     num_workers=2,
     persistent_workers=True,
     sampler=dict(type='DefaultSampler', shuffle=True),
@@ -73,8 +68,9 @@ train_dataloader = dict(
         ann_file=ann_file_train,
         data_prefix=dict(video=data_root),
         pipeline=train_pipeline))
+
 val_dataloader = dict(
-    batch_size=32,
+    batch_size=8,
     num_workers=2,
     persistent_workers=True,
     sampler=dict(type='DefaultSampler', shuffle=False),
@@ -84,7 +80,6 @@ val_dataloader = dict(
         data_prefix=dict(video=data_root_val),
         pipeline=val_pipeline,
         test_mode=True))
-
 test_dataloader = dict(
     batch_size=1,
     num_workers=2,
@@ -100,7 +95,6 @@ test_dataloader = dict(
 val_evaluator = dict(type='AccMetric')
 test_evaluator = val_evaluator
 
-# Save checkpoints every epoch, and only keep the latest checkpoint
 default_hooks = dict(
     checkpoint=dict(type='CheckpointHook', interval=1, max_keep_ckpts=1))
 
@@ -116,9 +110,8 @@ param_scheduler = [
         gamma=0.1)
 ]
 
-
 # Default setting for scaling LR automatically
 #   - `enable` means enable scaling LR automatically
 #       or not by default.
-#   - `base_batch_size` = (8 GPUs) x (32 samples per GPU).
+#   - `base_batch_size` = (8 GPUs) x (8 samples per GPU).
 auto_scale_lr = dict(enable=False, base_batch_size=32)
