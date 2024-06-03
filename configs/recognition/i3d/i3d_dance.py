e1ff858ab35a22c97a13cbac8e671dd592cd8d2e
Sophie Wu <swu.lee02@gmail.com>
2024-06-03 16:05:43 -0700

    adjust i3d cfg

--- configs/recognition/i3d/i3d_dance.py
+++ configs/recognition/i3d/i3d_dance.py
@@ -51,14 +51,14 @@ test_pipeline = [
     dict(type='DecordInit', **file_client_args),
     dict(
         type='SampleFrames',
-        clip_len=32,
-        frame_interval=2,
-        num_clips=10,
+        clip_len=1,
+        frame_interval=1,
+        num_clips=25,
         test_mode=True),
     dict(type='DecordDecode'),
     dict(type='Resize', scale=(-1, 256)),
-    dict(type='ThreeCrop', crop_size=256),
-    dict(type='FormatShape', input_format='NCTHW'),
+    dict(type='TenCrop', crop_size=224),
+    dict(type='FormatShape', input_format='NCHW'),
     dict(type='PackActionInputs')
 ]
 
