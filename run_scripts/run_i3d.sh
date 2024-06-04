0e8177dfc03a09a2b7dad9ef74f9b729f04aacb0
Sophie Wu <swu.lee02@gmail.com>
2024-06-03 18:14:21 -0700

    adjust run script

--- run_scripts/run_i3d.sh
+++ run_scripts/run_i3d.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 
 # Run training
 python tools/train.py configs/recognition/i3d/i3d_i3dpl_dance.py
@@ -12,11 +13,11 @@ python tools/train.py configs/recognition/i3d/i3d_i3dpl_heavy_dance.py
 python tools/test.py configs/recognition/i3d/i3d_i3dpl_heavy_dance.py \
     work_dirs/i3d_i3dpl_heavy_dance/best_acc_*.pth
 
-# Run training
-python tools/train.py configs/recognition/csn/ipcsn_dance.py
-# Run Testing
-python tools/test.py configs/recognition/csn/ipcsn_dance.py \
-    work_dirs/ipcsn_dance/best_acc_*.pth
+# # Run training
+# python tools/train.py configs/recognition/csn/ipcsn_dance.py
+# # Run Testing
+# python tools/test.py configs/recognition/csn/ipcsn_dance.py \
+#     work_dirs/ipcsn_dance/best_acc_*.pth
 
 
 # Shut down the VM after the training is complete
