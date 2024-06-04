67947ffb3453ca1ee8b2e4ee33d426e6d395ef2c
Sophie Wu <swu.lee02@gmail.com>
2024-06-03 17:54:17 -0700

    add ipcsn config

--- run_scripts/run_i3d.sh
+++ run_scripts/run_i3d.sh
@@ -13,16 +13,10 @@ python tools/test.py configs/recognition/i3d/i3d_i3dpl_heavy_dance.py \
     work_dirs/i3d_i3dpl_heavy_dance/best_acc_*.pth
 
 # Run training
-python tools/train.py configs/recognition/i3d/i3d_tsnpl_dance.py
+python tools/train.py configs/recognition/csn/ipcsn_dance.py
 # Run Testing
-python tools/test.py configs/recognition/i3d/i3d_tsnpl_dance.py \
-    work_dirs/i3d_tsnpl_dance/best_acc_*.pth
-
-# Run training
-python tools/train.py configs/recognition/i3d/i3d_tsnpl_heavy_dance.py
-# Run Testing
-python tools/test.py configs/recognition/i3d/i3d_tsnpl_heavy_dance.py \
-    work_dirs/i3d_tsnpl_heavy_dance/best_acc_*.pth
+python tools/test.py configs/recognition/csn/ipcsn_dance.py \
+    work_dirs/ipcsn_dance/best_acc_*.pth
 
 
 # Shut down the VM after the training is complete
