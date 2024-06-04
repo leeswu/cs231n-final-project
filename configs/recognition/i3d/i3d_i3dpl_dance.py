3001803963aaf4951c230a01948b95d038dff362
Sophie Wu <swu.lee02@gmail.com>
2024-06-03 17:33:47 -0700

    change num dataworkers

--- configs/recognition/i3d/i3d_i3dpl_dance.py
+++ configs/recognition/i3d/i3d_i3dpl_dance.py
@@ -63,7 +63,7 @@ test_pipeline = [
 
 train_dataloader = dict(
     batch_size=8,
-    num_workers=2,
+    num_workers=1,
     persistent_workers=True,
     sampler=dict(type='DefaultSampler', shuffle=True),
     dataset=dict(
@@ -74,7 +74,7 @@ train_dataloader = dict(
 
 val_dataloader = dict(
     batch_size=8,
-    num_workers=2,
+    num_workers=1,
     persistent_workers=True,
     sampler=dict(type='DefaultSampler', shuffle=False),
     dataset=dict(
