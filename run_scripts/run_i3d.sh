68d8022312e002ed3c70029c1d0eb1914c7079c4
Sophie Wu <swu.lee02@gmail.com>
2024-06-03 17:27:05 -0700

    update run script

--- run_scripts/run_i3d.sh
+++ run_scripts/run_i3d.sh
@@ -1,11 +1,29 @@
 #!/bin/bash
 
 # Run training
-python tools/train.py configs/recognition/i3d/i3d_dance.py
-
+python tools/train.py configs/recognition/i3d/i3d_i3dpl_dance.py
 # Run Testing
-python tools/test.py configs/recognition/i3d/i3d_dance.py \
-    work_dirs/i3d_dance.py/best_acc_*.pth
+python tools/test.py configs/recognition/i3d/i3d_i3dpl_dance.py \
+    work_dirs/i3d_i3dpl_dance/best_acc_*.pth
+
+# Run training
+python tools/train.py configs/recognition/i3d/i3d_i3dpl_heavy_dance.py
+# Run Testing
+python tools/test.py configs/recognition/i3d/i3d_i3dpl_heavy_dance.py \
+    work_dirs/i3d_i3dpl_heavy_dance/best_acc_*.pth
+
+# Run training
+python tools/train.py configs/recognition/i3d/i3d_tsnpl_dance.py
+# Run Testing
+python tools/test.py configs/recognition/i3d/i3d_tsnpl_dance.py \
+    work_dirs/i3d_tsnpl_dance/best_acc_*.pth
+
+# Run training
+python tools/train.py configs/recognition/i3d/i3d_tsnpl_heavy_dance.py
+# Run Testing
+python tools/test.py configs/recognition/i3d/i3d_tsnpl_heavy_dance.py \
+    work_dirs/i3d_tsnpl_heavy_dance/best_acc_*.pth
+
 
 # Shut down the VM after the training is complete
 sudo shutdown -h now
