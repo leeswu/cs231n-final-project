273d89fdd717068f3a4da5bc1d8c057b887ab98c
Sophie Wu <swu.lee02@gmail.com>
2024-06-03 17:55:34 -0700

    adjust num_workers

--- configs/recognition/csn/ipcsn_dance.py
+++ configs/recognition/csn/ipcsn_dance.py
@@ -63,7 +63,7 @@ test_pipeline = [
 
 train_dataloader = dict(
     batch_size=12,
-    num_workers=8,
+    num_workers=1,
     persistent_workers=True,
     sampler=dict(type='DefaultSampler', shuffle=True),
     dataset=dict(
@@ -74,7 +74,7 @@ train_dataloader = dict(
 
 val_dataloader = dict(
     batch_size=1,
-    num_workers=8,
+    num_workers=1,
     persistent_workers=True,
     sampler=dict(type='DefaultSampler', shuffle=False),
     dataset=dict(
@@ -86,7 +86,7 @@ val_dataloader = dict(
 
 test_dataloader = dict(
     batch_size=1,
-    num_workers=8,
+    num_workers=1,
     persistent_workers=True,
     sampler=dict(type='DefaultSampler', shuffle=False),
     dataset=dict(
